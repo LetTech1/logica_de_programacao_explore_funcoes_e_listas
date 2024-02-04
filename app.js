c59c138536e355a054a2aa74961f9ec3bc7c7efd
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-04 16:52:24 -0300

    Primeira aula do curso

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Insira um número de 1 a 10';
+
+function verificarChute(){
+    console.log('O botão foi clicado');
+}

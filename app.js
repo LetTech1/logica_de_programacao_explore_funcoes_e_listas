3e3e9627e67874cedb34be0e762f2ead5a37fca0
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-05 12:37:22 -0300

    Adicionando função com parâmetros no código

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let titulo = document.querySelector('h1');
+/* let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do Número Secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Insira um número de 1 a 10';
+paragrafo.innerHTML = 'Insira um número de 1 a 10'; */
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Insira um número de 1 a 100');
 
 function verificarChute(){
     console.log('O botão foi clicado');

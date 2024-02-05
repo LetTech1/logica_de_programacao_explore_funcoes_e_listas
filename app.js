2f24f231fcb3d7f15cdc854825700e6a6ee5e155
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-05 17:16:54 -0300

    Editando a função que verifica o número informado no chute

--- app.js
+++ app.js
@@ -16,7 +16,17 @@ exibirTextoNaTela('p', 'Insira um número de 1 a 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Você acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }else{
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+    }
 }
 
 function gerarNumeroAleatorio(){

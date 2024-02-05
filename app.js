9660a488f3abefb7b6f85c7264d9c7190359960c
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-05 12:45:10 -0300

    Adicionando uma função que retorna um valor

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ titulo.innerHTML = 'Jogo do Número Secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Insira um número de 1 a 10'; */
 
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -13,5 +15,9 @@ exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Insira um número de 1 a 100');
 
 function verificarChute(){
-    console.log('O botão foi clicado');
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
 }

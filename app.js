7316ee65784085abe5425ab22c2e54368732dd2d
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-07 20:40:07 -0300

    Removendo o número do campo quando está errado e habilitando o botão de Novo Jogo

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Insira um número de 1 a 10'; */
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -16,19 +17,29 @@ exibirTextoNaTela('p', 'Insira um número de 1 a 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemPositiva = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
     
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Você acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+        exibirTextoNaTela('p', mensagemPositiva);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O número secreto é menor');
         }else{
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+        tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 }
